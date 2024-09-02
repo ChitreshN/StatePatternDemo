0693e04f5872478031c10cbc84590b093f666e02
ChitreshN <chitreshnarra@gmail.com>
2024-09-03 01:26:23 +0530

    initial commit

---
+++ State.Tests/UnitTests1.cs
@@ -0,0 +1,17 @@
+using State.Structural;
+
+namespace State.UnitTests
+{
+    public class StateChange
+    {
+        [Fact]
+        public void StateChangeCheck()
+        {
+            var context = new Context(new ConcreteStateA());
+            context.Request();
+            Assert.Same(context.State.GetType(),typeof(ConcreteStateB));
+            context.Request();
+            Assert.Same(context.State.GetType(),typeof(ConcreteStateA));
+        }
+    }
+}
---
+++ StateDesignPattern/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace State.Structural
+{
+    /// <summary>
+    /// The 'State' abstract class
+    /// </summary>
+    public abstract class State
+    {
+        public abstract void Handle(Context context);
+    }
+
+    /// <summary>
+    /// A 'ConcreteState' class
+    /// </summary>
+
+    public class ConcreteStateA : State
+    {
+        public override void Handle(Context context)
+        {
+            // Changes state from A to B
+            context.State = new ConcreteStateB();
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteState' class
+    /// </summary>
+
+    public class ConcreteStateB : State
+    {
+        public override void Handle(Context context)
+        {
+            // Changes state from B to A
+            context.State = new ConcreteStateA();
+        }
+    }
+}
